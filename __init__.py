6fbe912f66a310331a5a3db06b8e88690bf9f71e
hemphen <michael.hemph@tabyenskilda.se>
2025-03-19 17:41:08 +0000

    Remove call to config_entry_first_refresh to avoid frequent retries

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     session = async_get_clientsession(hass)
     coordinator = CleverTouchUpdateCoordinator(hass, entry=entry, session=session)
-    await coordinator.async_config_entry_first_refresh()
+    await coordinator.async_refresh()
 
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][entry.entry_id] = coordinator
